b71be5aa1eb56d3b4c0d2466391b5f6d50b19636
Jay Johnson <jaysbeverage@gmail.com>
2018-08-02 01:28:50 -0700

    favicon

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ class App extends Component {
         <footer>
             <div class="footer-icon">
               <a href="https://www.linkedin.com/in/jay-johnson-4b577341/" class="link" target="_blank">
-                <i class="fab fa-linkedin-in linkedin"></i>
+                <i class="fab fa-linkedin"></i>
               </a>
               <a href="https://github.com/JJ1822" class="git" target="_blank">
                 <i class="fab fa-github github"></i>
               </a>
-              <a href="https://angel.co/jay-johnson-32" class="angel" target="_blank">
-                <i class="fab fa-angellist angellist"></i>
+              <a href="https://jayjohnson.info" class="angel" target="_blank">
+                <i class="fas fa-user-circle"></i>
               </a>
             </div>
             <div class="">
